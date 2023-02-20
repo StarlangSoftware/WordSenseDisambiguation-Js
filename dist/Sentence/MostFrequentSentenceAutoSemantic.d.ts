8f656cde7111711c858ffe8f88fc5913f89dc490
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-02-20 21:52:44 +0300

    Updated MostFrequentSentenceAutoSemantic.

--- dist/Sentence/MostFrequentSentenceAutoSemantic.d.ts
+++ dist/Sentence/MostFrequentSentenceAutoSemantic.d.ts
@@ -3,8 +3,24 @@ import { AnnotatedSentence } from "nlptoolkit-annotatedsentence/dist/AnnotatedSe
 import { WordNet } from "nlptoolkit-wordnet/dist/WordNet";
 import { FsmMorphologicalAnalyzer } from "nlptoolkit-morphologicalanalysis/dist/MorphologicalAnalysis/FsmMorphologicalAnalyzer";
 export declare class MostFrequentSentenceAutoSemantic extends SentenceAutoSemantic {
-    private turkishWordNet;
-    private fsm;
+    private readonly turkishWordNet;
+    private readonly fsm;
     constructor(turkishWordNet: WordNet, fsm: FsmMorphologicalAnalyzer);
+    /**
+     Determines the synset containing the literal with the lowest sense number.
+     @param literals an ArrayList of Literal objects
+     @return the SynSet containing the literal with the lowest sense number, or null if the input list is empty
+     */
+    private mostFrequentOfLiterals;
+    /**
+     * Checks
+     * 1. the previous two words and the current word; the previous, current and next word, current and the next
+     * two words for a three word multiword expression that occurs in the Turkish wordnet.
+     * 2. the previous word and current word; current word and the next word for a two word multiword expression that
+     * occurs in the Turkish wordnet.
+     * 3. the current word
+     * and sets the most frequent sense for that multiword expression or word.
+     * @param sentence The sentence for which word sense disambiguation will be determined automatically.
+     */
     protected autoLabelSingleSemantics(sentence: AnnotatedSentence): boolean;
 }
--- source/AutoProcessor/Sentence/MostFrequentSentenceAutoSemantic.ts
+++ source/AutoProcessor/Sentence/MostFrequentSentenceAutoSemantic.ts
@@ -5,11 +5,13 @@ import {
     FsmMorphologicalAnalyzer
 } from "nlptoolkit-morphologicalanalysis/dist/MorphologicalAnalysis/FsmMorphologicalAnalyzer";
 import {AnnotatedWord} from "nlptoolkit-annotatedsentence/dist/AnnotatedWord";
+import {SynSet} from "nlptoolkit-wordnet/dist/SynSet";
+import {Literal} from "nlptoolkit-wordnet/dist/Literal";
 
 export class MostFrequentSentenceAutoSemantic extends SentenceAutoSemantic{
 
-    private turkishWordNet: WordNet;
-    private fsm: FsmMorphologicalAnalyzer;
+    private readonly turkishWordNet: WordNet;
+    private readonly fsm: FsmMorphologicalAnalyzer;
 
     constructor(turkishWordNet: WordNet, fsm: FsmMorphologicalAnalyzer) {
         super();
@@ -17,17 +19,149 @@ export class MostFrequentSentenceAutoSemantic extends SentenceAutoSemantic{
         this.fsm = fsm
     }
 
+    /**
+     Determines the synset containing the literal with the lowest sense number.
+     @param literals an ArrayList of Literal objects
+     @return the SynSet containing the literal with the lowest sense number, or null if the input list is empty
+     */
+    private mostFrequentOfLiterals(literals: Array<Literal>): SynSet{
+        if (literals.length == 1){
+            return this.turkishWordNet.getSynSetWithId(literals[0].getSynSetId());
+        }
+        let minSense = 50;
+        let best = null;
+        for (let literal of literals){
+            if (literal.getSense() < minSense){
+                minSense = literal.getSense();
+                best = this.turkishWordNet.getSynSetWithId(literal.getSynSetId());
+            }
+        }
+        return best;
+    }
+
+    /**
+     * Checks
+     * 1. the previous two words and the current word; the previous, current and next word, current and the next
+     * two words for a three word multiword expression that occurs in the Turkish wordnet.
+     * 2. the previous word and current word; current word and the next word for a two word multiword expression that
+     * occurs in the Turkish wordnet.
+     * 3. the current word
+     * and sets the most frequent sense for that multiword expression or word.
+     * @param sentence The sentence for which word sense disambiguation will be determined automatically.
+     */
     protected autoLabelSingleSemantics(sentence: AnnotatedSentence): boolean {
+        let done = false;
+        let twoPrevious = null, previous = null, current, twoNext = null, next = null;
         for (let i = 0; i < sentence.wordCount(); i++) {
-            let synSets = this.getCandidateSynSets(this.turkishWordNet, this.fsm, sentence, i);
-            if (synSets.length > 0){
-                let best = this.mostFrequent(synSets, (<AnnotatedWord> sentence.getWord(i)).getParse().getWord().getName());
-                if (best != null){
-                    (<AnnotatedWord> sentence.getWord(i)).setSemantic(best.getId());
+            current = sentence.getWord(i) as AnnotatedWord;
+            if (i > 1) {
+                twoPrevious = sentence.getWord(i - 2) as AnnotatedWord;
+            }
+            if (i > 0) {
+                previous = sentence.getWord(i - 1) as AnnotatedWord;
+            }
+            if (i != sentence.wordCount() - 1) {
+                next = sentence.getWord(i + 1) as AnnotatedWord;
+            }
+            if (i < sentence.wordCount() - 2) {
+                twoNext = sentence.getWord(i + 2) as AnnotatedWord;
+            }
+            if (current.getSemantic() == null && current.getParse() != null) {
+                if (twoPrevious != null && twoPrevious.getParse() != null && previous.getParse() != null) {
+                    let literals = this.turkishWordNet.constructIdiomLiterals(this.fsm,
+                        twoPrevious.getParse(),
+                        twoPrevious.getMetamorphicParse(),
+                        previous.getParse(),
+                        previous.getMetamorphicParse(),
+                        current.getParse(),
+                        current.getMetamorphicParse());
+                    if (literals.length > 0) {
+                        let bestSynset = this.mostFrequentOfLiterals(literals);
+                        if (bestSynset != null){
+                            current.setSemantic(bestSynset.getId());
+                            done = true;
+                            continue;
+                        }
+                    }
+                }
+                if (previous != null && previous.getParse() != null && next != null && next.getParse() != null) {
+                    let literals = this.turkishWordNet.constructIdiomLiterals(this.fsm,
+                        previous.getParse(),
+                        previous.getMetamorphicParse(),
+                        current.getParse(),
+                        current.getMetamorphicParse(),
+                        next.getParse(),
+                        next.getMetamorphicParse());
+                    if (literals.length > 0) {
+                        let bestSynset = this.mostFrequentOfLiterals(literals);
+                        if (bestSynset != null) {
+                            current.setSemantic(bestSynset.getId());
+                            done = true;
+                            continue;
+                        }
+                    }
+                }
+                if (next != null && next.getParse() != null && twoNext != null && twoNext.getParse() != null) {
+                    let literals = this.turkishWordNet.constructIdiomLiterals(this.fsm,
+                        current.getParse(),
+                        current.getMetamorphicParse(),
+                        next.getParse(),
+                        next.getMetamorphicParse(),
+                        twoNext.getParse(),
+                        twoNext.getMetamorphicParse());
+                    if (literals.length > 0) {
+                        let bestSynset = this.mostFrequentOfLiterals(literals);
+                        if (bestSynset != null) {
+                            current.setSemantic(bestSynset.getId());
+                            done = true;
+                            continue;
+                        }
+                    }
+                }
+                if (previous != null && previous.getParse() != null) {
+                    let literals = this.turkishWordNet.constructIdiomLiterals(this.fsm,
+                        previous.getParse(),
+                        previous.getMetamorphicParse(),
+                        current.getParse(),
+                        current.getMetamorphicParse());
+                    if (literals.length > 0) {
+                        let bestSynset = this.mostFrequentOfLiterals(literals);
+                        if (bestSynset != null) {
+                            current.setSemantic(bestSynset.getId());
+                            done = true;
+                            continue;
+                        }
+                    }
+                }
+                if (current.getSemantic() == null && next != null && next.getParse() != null) {
+                    let literals = this.turkishWordNet.constructIdiomLiterals(this.fsm,
+                        current.getParse(),
+                        current.getMetamorphicParse(),
+                        next.getParse(),
+                        next.getMetamorphicParse());
+                    if (literals.length > 0) {
+                        let bestSynset = this.mostFrequentOfLiterals(literals);
+                        if (bestSynset != null) {
+                            current.setSemantic(bestSynset.getId());
+                            done = true;
+                            continue;
+                        }
+                    }
+                }
+                let literals = this.turkishWordNet.constructLiterals(current.getParse().getWord().getName(),
+                    current.getParse(),
+                    current.getMetamorphicParse(),
+                    this.fsm);
+                if (current.getSemantic() == null && literals.length > 0) {
+                    let bestSynset = this.mostFrequentOfLiterals(literals);
+                    if (bestSynset != null) {
+                        current.setSemantic(bestSynset.getId());
+                        done = true;
+                    }
                 }
             }
         }
-        return true;
+        return done;
     }
 
 }
